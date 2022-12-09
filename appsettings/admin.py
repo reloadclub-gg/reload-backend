4f1cc1f6f61cad558a61e10235f25ce3b44237e6
Adriano Margarin <Adriano Margarin>
2022-12-08 23:00:51 -0300

    Add app settings to configuration application

---
+++ appsettings/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import AppSettings
+
+@admin.register(AppSettings)
+class AppSettingsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'value']
+    list_display_links = ['id', 'name']
+
--- core/settings.py
+++ core/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'storages',
     'accounts.apps.AccountsConfig',
     'matchmaking.apps.MatchmakingConfig',
+    'appsettings.apps.AppSettingsConfig'
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
