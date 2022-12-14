7bc796f64edf23b8e4f12908d15c103753f4958d
Adriano Margarin <Adriano Margarin>
2022-12-13 21:39:29 -0300

    add route to set_public lobby

--- matchmaking/api/routes.py
+++ matchmaking/api/routes.py
@@ -8,3 +8,8 @@ router = Router(tags=['mm'])
 @router.patch('lobby/leave/', auth=AuthBearer())
 def lobby_leave(request):
     return Lobby.move(request.user.id)
+
+
+@router.patch('lobby/set-public/', auth=AuthBearer())
+def lobby_set_public(request):
+    return request.user.account.lobby.set_public()
