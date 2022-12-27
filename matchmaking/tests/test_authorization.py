fc716144adaa6a480bda62ce9943c36e2b7e600b
Adriano Margarin <adriano.margarin@gmail.com>
2022-12-26 22:46:18 -0300

    Criar autorizações mais utilizadas para os endpoints de mm (#64)
    * Add method and decorator to verified lobby owner
    
    * Add method and decorator to verified lobby participant
    
    * Update docstring decorator participant_required
    
    * Lint update
    
    Co-authored-by: Adriano Margarin <Adriano Margarin>

---
+++ matchmaking/tests/test_authorization.py
@@ -0,0 +1,34 @@
+from core.tests import TestCase
+from ..models import Lobby
+from ..api.authorization import is_lobby_owner, is_lobby_participant
+from . import mixins
+
+
+class AuthorizationTestCase(mixins.SomePlayersMixin, TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.online_verified_user_1.auth.add_session()
+        self.online_verified_user_2.auth.add_session()
+
+    def test_is_lobby_owner_is_true(self):
+        lobby = Lobby.create(self.online_verified_user_1.id)
+
+        self.assertTrue(is_lobby_owner(self.online_verified_user_1, lobby.id))
+
+    def test_is_lobby_owner_is_false(self):
+        lobby = Lobby.create(self.online_verified_user_1.id)
+
+        self.assertFalse(is_lobby_owner(self.online_verified_user_2, lobby.id))
+
+    def test_is_lobby_participant_is_true(self):
+        lobby_1 = Lobby.create(self.online_verified_user_1.id)
+        lobby_2 = Lobby.create(self.online_verified_user_2.id)
+        lobby_1.invite(lobby_2.id)
+        Lobby.move(lobby_2.id, lobby_1.id)
+
+        self.assertTrue(is_lobby_participant(self.online_verified_user_2, lobby_1.id))
+
+    def test_is_lobby_participant_is_false(self):
+        lobby = Lobby.create(self.online_verified_user_1.id)
+
+        self.assertFalse(is_lobby_participant(self.online_verified_user_2, lobby.id))
