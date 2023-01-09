35c5b26140428f2293abc9e30831c707c79829f0
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-01-09 14:26:41 -0300

    add ws lobby_player_leave tests

--- websocket/tests/test_controller.py
+++ websocket/tests/test_controller.py
@@ -1,5 +1,6 @@
 from unittest import mock
 
+from matchmaking.models import Lobby
 from matchmaking.tests.mixins import VerifiedPlayersMixin
 from core.tests import TestCase
 from websocket import controller
@@ -31,7 +32,7 @@ class WSControllerTestCase(VerifiedPlayersMixin, TestCase):
         self.assertEqual(mocker.await_count, 1)
 
     @mock.patch('websocket.utils.send_and_close')
-    def test_user_status_change_no_online_friend(self, mocker):
+    def test_user_status_change_no_online_friends(self, mocker):
         self.user_1.auth.add_session()
 
         mocker.return_value = True
@@ -60,9 +61,60 @@ class WSControllerTestCase(VerifiedPlayersMixin, TestCase):
         self.assertEqual(mocker.await_count, 1)
 
     @mock.patch('websocket.utils.send_and_close')
-    def test_friendlist_add_no_online_friend(self, mocker):
+    def test_friendlist_add_no_online_friends(self, mocker):
         self.user_1.auth.add_session()
 
         mocker.return_value = True
         controller.friendlist_add(self.user_1)
+        mocker.assert_not_awaited()
+
+    @mock.patch('websocket.utils.send_and_close')
+    def test_lobby_player_leave_2_remaining_players(self, mocker):
+        self.user_1.auth.add_session()
+        self.user_2.auth.add_session()
+        self.user_3.auth.add_session()
+
+        Lobby.create(self.user_1.id)
+        Lobby.create(self.user_2.id)
+        Lobby.create(self.user_3.id)
+
+        self.user_1.account.lobby.set_public()
+        Lobby.move(self.user_2.id, self.user_1.account.lobby.id)
+        Lobby.move(self.user_3.id, self.user_1.account.lobby.id)
+
+        mocker.return_value = True
+        controller.lobby_player_leave(self.user_3, self.user_1.account.lobby)
+        mocker.assert_awaited()
+        self.assertEqual(mocker.await_count, 2)
+
+    @mock.patch('websocket.utils.send_and_close')
+    def test_lobby_player_leave_1_remaining_player(self, mocker):
+        self.user_1.auth.add_session()
+        self.user_2.auth.add_session()
+        self.user_3.auth.add_session()
+
+        Lobby.create(self.user_1.id)
+        Lobby.create(self.user_2.id)
+        Lobby.create(self.user_3.id)
+
+        self.user_1.account.lobby.set_public()
+        Lobby.move(self.user_2.id, self.user_1.account.lobby.id)
+
+        mocker.return_value = True
+        controller.lobby_player_leave(self.user_2, self.user_1.account.lobby)
+        mocker.assert_awaited()
+        self.assertEqual(mocker.await_count, 1)
+
+    @mock.patch('websocket.utils.send_and_close')
+    def test_lobby_player_leave_no_remaining_players(self, mocker):
+        self.user_1.auth.add_session()
+        self.user_2.auth.add_session()
+        self.user_3.auth.add_session()
+
+        Lobby.create(self.user_1.id)
+        Lobby.create(self.user_2.id)
+        Lobby.create(self.user_3.id)
+
+        mocker.return_value = True
+        controller.lobby_player_leave(self.user_1, self.user_1.account.lobby)
         mocker.assert_not_awaited()
