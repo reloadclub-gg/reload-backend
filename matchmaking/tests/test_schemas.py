5c3b0c2ed7c58ee1c8094f0a37289b376e07d988
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-01-09 00:57:59 -0300

    add more fields to LobbySchema

--- matchmaking/tests/test_schemas.py
+++ matchmaking/tests/test_schemas.py
@@ -4,38 +4,53 @@ from ..api import schemas
 from ..models import Lobby
 
 
-class LobbySchemaTestCase(mixins.SomePlayersMixin, TestCase):
+class LobbySchemaTestCase(mixins.VerifiedPlayersMixin, TestCase):
     def setUp(self) -> None:
         super().setUp()
-        self.online_verified_user_1.auth.add_session()
-        self.online_verified_user_2.auth.add_session()
+        self.user_1.auth.add_session()
+        self.user_2.auth.add_session()
 
     def test_lobby_schema_as_dict(self):
-        lobby = Lobby.create(self.online_verified_user_1.id)
+        lobby = Lobby.create(self.user_1.id)
         payload = schemas.LobbySchema.from_orm(lobby).dict()
 
         expected_payload = {
-            'id': lobby.id,
-            'owner_id': lobby.owner_id,
-            'players_ids': [self.online_verified_user_1.id],
+            'id': self.user_1.id,
+            'owner_id': self.user_1.id,
+            'lobby_type': 'competitive',
+            'mode': 5,
+            'max_players': 5,
+            'players_ids': [self.user_1.id],
+            'is_public': False,
+            'invites': [],
+            'invited_players_ids': [],
+            'overall': 0,
+            'queue': None,
+            'queue_time': None,
         }
 
         self.assertDictEqual(payload, expected_payload)
 
     def test_lobby_schema_with_two_player_as_dict(self):
-        lobby_1 = Lobby.create(self.online_verified_user_1.id)
-        lobby_2 = Lobby.create(self.online_verified_user_2.id)
-        lobby_1.invite(lobby_2.id)
-        Lobby.move(lobby_2.id, lobby_1.id)
+        lobby_1 = Lobby.create(self.user_1.id)
+        lobby_2 = Lobby.create(self.user_2.id)
+        lobby_1.invite(self.user_1.id, self.user_2.id)
+        Lobby.move(self.user_2.id, lobby_1.id)
         payload = schemas.LobbySchema.from_orm(lobby_1).dict()
 
         expected_payload = {
-            'id': lobby_1.id,
-            'owner_id': lobby_1.owner_id,
-            'players_ids': [
-                self.online_verified_user_1.id,
-                self.online_verified_user_2.id,
-            ],
+            'id': self.user_1.id,
+            'owner_id': self.user_1.id,
+            'lobby_type': 'competitive',
+            'mode': 5,
+            'max_players': 5,
+            'players_ids': [self.user_1.id, self.user_2.id],
+            'is_public': False,
+            'invites': [],
+            'invited_players_ids': [],
+            'overall': 0,
+            'queue': None,
+            'queue_time': None,
         }
 
         self.assertDictEqual(payload, expected_payload)
