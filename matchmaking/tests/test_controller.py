ff24409740dc744f2a66d6d36db1f2a21e7edf58
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-01-09 00:35:44 -0300

    87 sistema de convites mais controlado (#88)
    * Add _MAP.md
    
    * rename and finish ws workflow mapping
    
    * add logout call to inactivate method
    
    * reorder steps for inactivation
    
    * update lobby invites to contain from and to ids
    
    Lobby invites now are represented like this from_user_id:to_user_id;
    Update invites property method to return a list of invite_ids (str);
    Add invited_players_ids property method to get all invited players ids
    (int);
    Update move method to reflect new invites logic;
    Update invite method by changing the received params to reflect new
    naming convetion;
    Add get_invite_by_to_player_id method to retrieve the invite_id for a
    specific player;
    Update delete_invite method to reflect new invite naming convetion and
    logic;
    
    * update lobby invite controller methods to reflect new invite logic
    
    * update routes to reflect new invite logic
    
    * add new mixin to simplify access to users on tests
    
    * update tests to reflect new invites logic and naming convetion
    
    * update move method to delete every invite created by the user is leaving
    
    * delete all invites on lobby queue start
    
    Co-authored-by: Adriano Margarin <adriano.margarin@gmail.com>

--- matchmaking/tests/test_controller.py
+++ matchmaking/tests/test_controller.py
@@ -7,77 +7,75 @@ from ..api import controller
 from ..models import Lobby
 
 
-class LobbyControllerTestCase(mixins.SomePlayersMixin, TestCase):
+class LobbyControllerTestCase(mixins.VerifiedPlayersMixin, TestCase):
     def setUp(self) -> None:
         super().setUp()
-        self.online_verified_user_1.auth.add_session()
-        self.online_verified_user_2.auth.add_session()
+        self.user_1.auth.add_session()
+        self.user_2.auth.add_session()
 
     def test_lobby_remove(self):
-        lobby_1 = Lobby.create(self.online_verified_user_1.id)
-        lobby_2 = Lobby.create(self.online_verified_user_2.id)
+        lobby_1 = Lobby.create(self.user_1.id)
+        lobby_2 = Lobby.create(self.user_2.id)
 
-        lobby_1.invite(lobby_2.id)
-        Lobby.move(lobby_2.id, self.online_verified_user_1.id)
+        lobby_1.invite(lobby_1.id, lobby_2.id)
+        Lobby.move(lobby_2.id, self.user_1.id)
 
         self.assertEqual(lobby_1.players_count, 2)
         self.assertEqual(lobby_2.players_count, 0)
 
         controller.lobby_remove(
-            request_user_id=self.online_verified_user_1.id,
+            request_user_id=self.user_1.id,
             lobby_id=lobby_1.id,
-            user_id=self.online_verified_user_2.id,
+            user_id=self.user_2.id,
         )
 
         self.assertEqual(lobby_1.players_count, 1)
         self.assertEqual(lobby_2.players_count, 1)
 
     def test_lobby_invite(self):
-        lobby_1 = Lobby.create(self.online_verified_user_1.id)
-        lobby_2 = Lobby.create(self.online_verified_user_2.id)
+        lobby_1 = Lobby.create(self.user_1.id)
+        lobby_2 = Lobby.create(self.user_2.id)
 
         self.assertEqual(lobby_1.players_count, 1)
         self.assertEqual(lobby_2.players_count, 1)
 
         controller.lobby_invite(
-            user=self.online_verified_user_1,
+            user=self.user_1,
             lobby_id=lobby_1.id,
-            player_id=self.online_verified_user_2.id,
+            player_id=self.user_2.id,
         )
 
-        self.assertEqual(lobby_1.invites, [lobby_2.id])
+        self.assertEqual(lobby_1.invites, [f'{self.user_1.id}:{self.user_2.id}'])
 
     def test_lobby_refuse_invite(self):
-        lobby_1 = Lobby.create(self.online_verified_user_1.id)
-        lobby_2 = Lobby.create(self.online_verified_user_2.id)
-        lobby_2.invite(lobby_1.id)
+        lobby_1 = Lobby.create(self.user_1.id)
+        lobby_2 = Lobby.create(self.user_2.id)
+        lobby_2.invite(lobby_2.id, lobby_1.id)
 
-        self.assertListEqual(lobby_2.invites, [lobby_1.id])
+        self.assertListEqual(lobby_2.invites, [f'{self.user_2.id}:{self.user_1.id}'])
         self.assertEqual(lobby_1.players_count, 1)
 
         controller.lobby_refuse_invite(
-            user=self.online_verified_user_1, lobby_id=lobby_2.id
+            lobby_id=lobby_2.id, invite_id=f'{self.user_2.id}:{self.user_1.id}'
         )
 
         self.assertListEqual(lobby_1.invites, [])
         self.assertEqual(lobby_1.players_count, 1)
 
     def test_lobby_change_type_and_mode(self):
-        lobby = Lobby.create(self.online_verified_user_1.id)
+        lobby = Lobby.create(self.user_1.id)
 
         self.assertEqual(lobby.mode, 5)
         self.assertEqual(lobby.lobby_type, 'competitive')
 
-        controller.lobby_change_type_and_mode(
-            self.online_verified_user_1, lobby.id, 'custom', 20
-        )
+        controller.lobby_change_type_and_mode(self.user_1, lobby.id, 'custom', 20)
 
         self.assertEqual(lobby.mode, 20)
         self.assertEqual(lobby.lobby_type, 'custom')
 
     def test_lobby_change_type_and_mode_by_non_owner(self):
-        lobby = Lobby.create(self.online_verified_user_1.id)
-        Lobby.create(self.online_verified_user_2.id)
+        lobby = Lobby.create(self.user_1.id)
+        Lobby.create(self.user_2.id)
 
         self.assertEqual(lobby.mode, 5)
         self.assertEqual(lobby.lobby_type, 'competitive')
@@ -85,16 +83,14 @@ class LobbyControllerTestCase(mixins.SomePlayersMixin, TestCase):
         with self.assertRaisesMessage(
             HttpError, 'User must be owner to perfom this action'
         ):
-            controller.lobby_change_type_and_mode(
-                self.online_verified_user_2, lobby.id, 'custom', 20
-            )
+            controller.lobby_change_type_and_mode(self.user_2, lobby.id, 'custom', 20)
 
     @mock.patch(
         'matchmaking.models.Lobby.queue',
         return_value=datetime.now(),
     )
     def test_lobby_change_type_and_mode_with_set_type_exception(self, _mock):
-        lobby = Lobby.create(self.online_verified_user_1.id)
+        lobby = Lobby.create(self.user_1.id)
 
         self.assertEqual(lobby.mode, 5)
         self.assertEqual(lobby.lobby_type, 'competitive')
@@ -102,26 +98,24 @@ class LobbyControllerTestCase(mixins.SomePlayersMixin, TestCase):
         with self.assertRaisesMessage(
             HttpError, 'Lobby is queued caught on set lobby type'
         ):
-            controller.lobby_change_type_and_mode(
-                self.online_verified_user_1, lobby.id, 'custom', 20
-            )
+            controller.lobby_change_type_and_mode(self.user_1, lobby.id, 'custom', 20)
 
     def test_lobby_enter(self):
-        lobby_1 = Lobby.create(self.online_verified_user_1.id)
+        lobby_1 = Lobby.create(self.user_1.id)
         lobby_1.set_public()
-        lobby_2 = Lobby.create(self.online_verified_user_2.id)
+        lobby_2 = Lobby.create(self.user_2.id)
 
         self.assertEqual(lobby_1.players_count, 1)
         self.assertEqual(lobby_2.players_count, 1)
 
-        controller.lobby_enter(self.online_verified_user_2, lobby_1.id)
+        controller.lobby_enter(self.user_2, lobby_1.id)
 
         self.assertEqual(lobby_1.players_count, 2)
         self.assertEqual(lobby_2.players_count, 0)
 
     def test_lobby_enter_isnt_public(self):
-        lobby_1 = Lobby.create(self.online_verified_user_1.id)
-        lobby_2 = Lobby.create(self.online_verified_user_2.id)
+        lobby_1 = Lobby.create(self.user_1.id)
+        lobby_2 = Lobby.create(self.user_2.id)
 
         self.assertEqual(lobby_1.players_count, 1)
         self.assertEqual(lobby_2.players_count, 1)
@@ -129,40 +123,40 @@ class LobbyControllerTestCase(mixins.SomePlayersMixin, TestCase):
         with self.assertRaisesMessage(
             HttpError, 'User not invited caught on lobby move'
         ):
-            controller.lobby_enter(self.online_verified_user_2, lobby_1.id)
+            controller.lobby_enter(self.user_2, lobby_1.id)
 
     def test_set_public(self):
-        lobby = Lobby.create(self.online_verified_user_1.id)
-        lobby_returned = controller.set_public(self.online_verified_user_1)
+        lobby = Lobby.create(self.user_1.id)
+        lobby_returned = controller.set_public(self.user_1)
 
         self.assertEqual(lobby_returned, lobby)
         self.assertTrue(lobby.is_public)
 
     def test_set_public_non_owner(self):
-        lobby_1 = Lobby.create(self.online_verified_user_1.id)
-        lobby_2 = Lobby.create(self.online_verified_user_2.id)
-        lobby_1.invite(lobby_2.id)
+        lobby_1 = Lobby.create(self.user_1.id)
+        lobby_2 = Lobby.create(self.user_2.id)
+        lobby_1.invite(lobby_1.id, lobby_2.id)
         Lobby.move(lobby_2.id, lobby_1.id)
 
         with self.assertRaisesMessage(
             HttpError, 'User must be owner to perfom this action'
         ):
-            controller.set_public(self.online_verified_user_2)
+            controller.set_public(self.user_2)
 
     def test_set_private(self):
-        lobby = Lobby.create(self.online_verified_user_1.id)
-        lobby_returned = controller.set_private(self.online_verified_user_1)
+        lobby = Lobby.create(self.user_1.id)
+        lobby_returned = controller.set_private(self.user_1)
 
         self.assertEqual(lobby_returned, lobby)
         self.assertFalse(lobby.is_public)
 
     def test_set_private_non_owner(self):
-        lobby_1 = Lobby.create(self.online_verified_user_1.id)
-        lobby_2 = Lobby.create(self.online_verified_user_2.id)
-        lobby_1.invite(lobby_2.id)
+        lobby_1 = Lobby.create(self.user_1.id)
+        lobby_2 = Lobby.create(self.user_2.id)
+        lobby_1.invite(lobby_1.id, lobby_2.id)
         Lobby.move(lobby_2.id, lobby_1.id)
 
         with self.assertRaisesMessage(
             HttpError, 'User must be owner to perfom this action'
         ):
-            controller.set_private(self.online_verified_user_2)
+            controller.set_private(self.user_2)
--- matchmaking/tests/test_routes.py
+++ matchmaking/tests/test_routes.py
@@ -3,56 +3,54 @@ from ..models import Lobby
 from . import mixins
 
 
-class LobbyAPITestCase(mixins.SomePlayersMixin, TestCase):
+class LobbyAPITestCase(mixins.VerifiedPlayersMixin, TestCase):
     def setUp(self) -> None:
         self.api = APIClient('/api/mm')
         super().setUp()
-        self.online_verified_user_1.auth.add_session()
-        self.online_verified_user_1.auth.create_token()
-        self.online_verified_user_2.auth.add_session()
-        self.online_verified_user_3.auth.add_session()
-        self.online_verified_user_3.auth.create_token()
-        self.online_verified_user_2.auth.create_token()
+        self.user_1.auth.add_session()
+        self.user_1.auth.create_token()
+        self.user_2.auth.add_session()
+        self.user_3.auth.add_session()
+        self.user_3.auth.create_token()
+        self.user_2.auth.create_token()
 
     def test_lobby_leave(self):
-        lobby_1 = Lobby.create(self.online_verified_user_1.id)
-        lobby_2 = Lobby.create(self.online_verified_user_2.id)
-        lobby_2.invite(self.online_verified_user_1.id)
+        lobby_1 = Lobby.create(self.user_1.id)
+        lobby_2 = Lobby.create(self.user_2.id)
+        lobby_2.invite(self.user_2.id, self.user_1.id)
 
-        Lobby.move(self.online_verified_user_1.id, lobby_2.id)
+        Lobby.move(self.user_1.id, lobby_2.id)
 
         self.assertEqual(lobby_1.players_count, 0)
         self.assertEqual(lobby_2.players_count, 2)
 
-        response = self.api.call(
-            'patch', '/lobby/leave', token=self.online_verified_user_1.auth.token
-        )
+        response = self.api.call('patch', '/lobby/leave', token=self.user_1.auth.token)
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(lobby_1.players_count, 1)
         self.assertEqual(lobby_2.players_count, 1)
 
     def test_lobby_set_public(self):
-        lobby = Lobby.create(self.online_verified_user_1.id)
+        lobby = Lobby.create(self.user_1.id)
         self.assertFalse(lobby.is_public)
 
         response = self.api.call(
-            'patch', '/lobby/set-public', token=self.online_verified_user_1.auth.token
+            'patch', '/lobby/set-public', token=self.user_1.auth.token
         )
 
         self.assertEqual(response.status_code, 200)
         self.assertTrue(lobby.is_public)
 
     def test_lobby_set_public_non_owner(self):
-        lobby = Lobby.create(self.online_verified_user_1.id)
-        Lobby.create(self.online_verified_user_2.id)
-        lobby.invite(self.online_verified_user_2.id)
-        Lobby.move(self.online_verified_user_2.id, lobby.id)
+        lobby = Lobby.create(self.user_1.id)
+        Lobby.create(self.user_2.id)
+        lobby.invite(self.user_1.id, self.user_2.id)
+        Lobby.move(self.user_2.id, lobby.id)
 
         self.assertFalse(lobby.is_public)
 
         response = self.api.call(
-            'patch', '/lobby/set-public', token=self.online_verified_user_2.auth.token
+            'patch', '/lobby/set-public', token=self.user_2.auth.token
         )
 
         self.assertEqual(response.status_code, 400)
@@ -61,27 +59,27 @@ class LobbyAPITestCase(mixins.SomePlayersMixin, TestCase):
         )
 
     def test_lobby_set_private(self):
-        lobby = Lobby.create(self.online_verified_user_1.id)
+        lobby = Lobby.create(self.user_1.id)
         lobby.set_public()
         self.assertTrue(lobby.is_public)
 
         response = self.api.call(
-            'patch', '/lobby/set-private', token=self.online_verified_user_1.auth.token
+            'patch', '/lobby/set-private', token=self.user_1.auth.token
         )
 
         self.assertEqual(response.status_code, 200)
         self.assertFalse(lobby.is_public)
 
     def test_lobby_set_private_non_owner(self):
-        lobby = Lobby.create(self.online_verified_user_1.id)
+        lobby = Lobby.create(self.user_1.id)
         lobby.set_public()
-        Lobby.create(self.online_verified_user_2.id)
-        Lobby.move(self.online_verified_user_2.id, lobby.id)
+        Lobby.create(self.user_2.id)
+        Lobby.move(self.user_2.id, lobby.id)
 
         self.assertTrue(lobby.is_public)
 
         response = self.api.call(
-            'patch', '/lobby/set-private', token=self.online_verified_user_2.auth.token
+            'patch', '/lobby/set-private', token=self.user_2.auth.token
         )
 
         self.assertEqual(response.status_code, 400)
@@ -90,18 +88,18 @@ class LobbyAPITestCase(mixins.SomePlayersMixin, TestCase):
         )
 
     def test_lobby_remove(self):
-        lobby_1 = Lobby.create(self.online_verified_user_1.id)
-        lobby_2 = Lobby.create(self.online_verified_user_2.id)
-        lobby_1.invite(lobby_2.id)
-        Lobby.move(lobby_2.id, self.online_verified_user_1.id)
+        lobby_1 = Lobby.create(self.user_1.id)
+        lobby_2 = Lobby.create(self.user_2.id)
+        lobby_1.invite(self.user_1.id, self.user_2.id)
+        Lobby.move(lobby_2.id, self.user_1.id)
 
         self.assertEqual(lobby_1.players_count, 2)
         self.assertEqual(lobby_2.players_count, 0)
 
         response = self.api.call(
             'patch',
-            f'lobby/{lobby_1.id}/remove-player/{self.online_verified_user_2.id}/',
-            token=self.online_verified_user_1.auth.token,
+            f'lobby/{lobby_1.id}/remove-player/{self.user_2.id}/',
+            token=self.user_1.auth.token,
         )
 
         self.assertEqual(response.status_code, 200)
@@ -109,14 +107,14 @@ class LobbyAPITestCase(mixins.SomePlayersMixin, TestCase):
         self.assertEqual(lobby_2.players_count, 1)
 
     def test_remove_user_outside_lobby(self):
-        Lobby.create(self.online_verified_user_1.id)
-        Lobby.create(self.online_verified_user_2.id)
-        lobby = Lobby.create(self.online_verified_user_3.id)
+        Lobby.create(self.user_1.id)
+        Lobby.create(self.user_2.id)
+        lobby = Lobby.create(self.user_3.id)
 
         response = self.api.call(
             'patch',
-            f'lobby/{lobby.id}/remove-player/{self.online_verified_user_2.id}/',
-            token=self.online_verified_user_1.auth.token,
+            f'lobby/{lobby.id}/remove-player/{self.user_2.id}/',
+            token=self.user_1.auth.token,
         )
 
         self.assertEqual(response.status_code, 400)
@@ -125,20 +123,20 @@ class LobbyAPITestCase(mixins.SomePlayersMixin, TestCase):
         )
 
     def test_remove_user_by_non_owner(self):
-        lobby_1 = Lobby.create(self.online_verified_user_1.id)
-        lobby_2 = Lobby.create(self.online_verified_user_2.id)
-        lobby_3 = Lobby.create(self.online_verified_user_3.id)
+        lobby_1 = Lobby.create(self.user_1.id)
+        lobby_2 = Lobby.create(self.user_2.id)
+        lobby_3 = Lobby.create(self.user_3.id)
 
-        lobby_1.invite(lobby_2.id)
-        Lobby.move(lobby_2.id, self.online_verified_user_1.id)
+        lobby_1.invite(self.user_1.id, lobby_2.id)
+        Lobby.move(lobby_2.id, self.user_1.id)
 
-        lobby_1.invite(lobby_3.id)
-        Lobby.move(lobby_3.id, self.online_verified_user_1.id)
+        lobby_1.invite(self.user_1.id, lobby_3.id)
+        Lobby.move(lobby_3.id, self.user_1.id)
 
         response = self.api.call(
             'patch',
-            f'lobby/{lobby_1.id}/remove-player/{self.online_verified_user_2.id}/',
-            token=self.online_verified_user_3.auth.token,
+            f'lobby/{lobby_1.id}/remove-player/{self.user_2.id}/',
+            token=self.user_3.auth.token,
         )
 
         self.assertEqual(response.status_code, 400)
@@ -147,94 +145,46 @@ class LobbyAPITestCase(mixins.SomePlayersMixin, TestCase):
         )
 
     def test_lobby_invite(self):
-        lobby = Lobby.create(self.online_verified_user_1.id)
+        lobby = Lobby.create(self.user_1.id)
 
         response = self.api.call(
             'post',
-            f'lobby/{lobby.id}/invite-player/{self.online_verified_user_2.id}/',
-            token=self.online_verified_user_1.auth.token,
+            f'lobby/{lobby.id}/invite-player/{self.user_2.id}/',
+            token=self.user_1.auth.token,
         )
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(
-            self.online_verified_user_1.account.lobby.invites,
-            [self.online_verified_user_2.id],
-        )
-
-    def test_lobby_invite_user_by_non_owner(self):
-        lobby_1 = Lobby.create(self.online_verified_user_1.id)
-        lobby_2 = Lobby.create(self.online_verified_user_2.id)
-
-        response = self.api.call(
-            'post',
-            f'lobby/{lobby_2.id}/invite-player/{lobby_1.id}/',
-            token=self.online_verified_user_1.auth.token,
-        )
-
-        self.assertEqual(response.status_code, 400)
-        self.assertDictEqual(
-            response.json(), {'detail': 'User must be owner to perfom this action'}
+            self.user_1.account.lobby.invites,
+            [f'{self.user_1.id}:{self.user_2.id}'],
         )
 
     def test_lobby_invite_user_already_been_invited(self):
-        lobby_1 = Lobby.create(self.online_verified_user_1.id)
-        lobby_2 = Lobby.create(self.online_verified_user_2.id)
-        lobby_1.invite(lobby_2.id)
+        lobby_1 = Lobby.create(self.user_1.id)
+        lobby_2 = Lobby.create(self.user_2.id)
+        lobby_1.invite(self.user_1.id, lobby_2.id)
 
         response = self.api.call(
             'post',
             f'lobby/{lobby_1.id}/invite-player/{lobby_2.id}/',
-            token=self.online_verified_user_1.auth.token,
+            token=self.user_1.auth.token,
         )
 
         self.assertEqual(response.status_code, 400)
         self.assertDictEqual(
-            response.json(), {'detail': 'Player id has already been invited'}
+            response.json(), {'detail': 'User already invited caught on lobby invite'}
         )
-
-    def test_lobby_invite_user_already_in_lobby(self):
-        lobby_1 = Lobby.create(self.online_verified_user_1.id)
-        lobby_2 = Lobby.create(self.online_verified_user_2.id)
-        lobby_1.invite(lobby_2.id)
-        lobby_1.move(lobby_2.id, lobby_1.id)
-
-        response = self.api.call(
-            'post',
-            f'lobby/{lobby_1.id}/invite-player/{lobby_2.id}/',
-            token=self.online_verified_user_1.auth.token,
-        )
-
-        self.assertEqual(response.status_code, 400)
-        self.assertDictEqual(response.json(), {'detail': 'User already in lobby'})
-
-    def test_lobby_accept_invite(self):
-        lobby = Lobby.create(self.online_verified_user_1.id)
-        lobby.invite(self.online_verified_user_2.id)
-
-        Lobby.create(self.online_verified_user_2.id)
-
-        self.assertListEqual(lobby.invites, [self.online_verified_user_2.id])
-
-        response = self.api.call(
-            'patch',
-            f'/lobby/{lobby.id}/accept-invite/',
-            token=self.online_verified_user_2.auth.token,
-        )
-
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(lobby.players_count, 2)
-        self.assertListEqual(lobby.invites, [])
 
     def test_lobby_accept_invite_has_no_invited(self):
-        lobby = Lobby.create(self.online_verified_user_1.id)
-        Lobby.create(self.online_verified_user_2.id)
+        lobby = Lobby.create(self.user_1.id)
+        Lobby.create(self.user_2.id)
 
         self.assertListEqual(lobby.invites, [])
 
         response = self.api.call(
             'patch',
-            f'/lobby/{lobby.id}/accept-invite/',
-            token=self.online_verified_user_2.auth.token,
+            f'/lobby/{lobby.id}/accept-invite/{self.user_1.id}:{self.user_2.id}/',
+            token=self.user_2.auth.token,
         )
 
         self.assertEqual(response.status_code, 400)
@@ -243,17 +193,17 @@ class LobbyAPITestCase(mixins.SomePlayersMixin, TestCase):
         )
 
     def test_lobby_refuse_invite(self):
-        lobby = Lobby.create(self.online_verified_user_1.id)
-        lobby.invite(self.online_verified_user_2.id)
+        lobby = Lobby.create(self.user_1.id)
+        lobby.invite(self.user_1.id, self.user_2.id)
 
-        Lobby.create(self.online_verified_user_2.id)
+        Lobby.create(self.user_2.id)
 
-        self.assertListEqual(lobby.invites, [self.online_verified_user_2.id])
+        self.assertListEqual(lobby.invites, [f'{self.user_1.id}:{self.user_2.id}'])
 
         response = self.api.call(
             'patch',
-            f'/lobby/{lobby.id}/refuse-invite/',
-            token=self.online_verified_user_2.auth.token,
+            f'/lobby/{lobby.id}/refuse-invite/{self.user_1.id}:{self.user_2.id}/',
+            token=self.user_2.auth.token,
         )
 
         self.assertEqual(response.status_code, 200)
@@ -261,15 +211,15 @@ class LobbyAPITestCase(mixins.SomePlayersMixin, TestCase):
         self.assertListEqual(lobby.invites, [])
 
     def test_lobby_refuse_invite_has_no_invited(self):
-        lobby = Lobby.create(self.online_verified_user_1.id)
-        Lobby.create(self.online_verified_user_2.id)
+        lobby = Lobby.create(self.user_1.id)
+        Lobby.create(self.user_2.id)
 
         self.assertListEqual(lobby.invites, [])
 
         response = self.api.call(
             'patch',
-            f'/lobby/{lobby.id}/refuse-invite/',
-            token=self.online_verified_user_2.auth.token,
+            f'/lobby/{lobby.id}/refuse-invite/{self.user_1.id}:{self.user_2.id}/',
+            token=self.user_2.auth.token,
         )
 
         self.assertEqual(response.status_code, 400)
@@ -278,7 +228,7 @@ class LobbyAPITestCase(mixins.SomePlayersMixin, TestCase):
         )
 
     def test_lobby_change_type_and_mode(self):
-        lobby = Lobby.create(self.online_verified_user_1.id)
+        lobby = Lobby.create(self.user_1.id)
 
         self.assertEqual(lobby.mode, 5)
         self.assertEqual(lobby.lobby_type, 'competitive')
@@ -290,7 +240,7 @@ class LobbyAPITestCase(mixins.SomePlayersMixin, TestCase):
         response = self.api.call(
             'patch',
             f'/lobby/{lobby_id}/change-type/{lobby_type}/change-mode/{lobby_mode}/',
-            token=self.online_verified_user_1.auth.token,
+            token=self.user_1.auth.token,
         )
 
         self.assertEqual(response.status_code, 200)
@@ -298,8 +248,8 @@ class LobbyAPITestCase(mixins.SomePlayersMixin, TestCase):
         self.assertEqual(lobby.lobby_type, 'custom')
 
     def test_lobby_change_type_and_mode_by_non_owner(self):
-        lobby = Lobby.create(self.online_verified_user_1.id)
-        Lobby.create(self.online_verified_user_2.id)
+        lobby = Lobby.create(self.user_1.id)
+        Lobby.create(self.user_2.id)
 
         self.assertEqual(lobby.mode, 5)
         self.assertEqual(lobby.lobby_type, 'competitive')
@@ -311,7 +261,7 @@ class LobbyAPITestCase(mixins.SomePlayersMixin, TestCase):
         response = self.api.call(
             'patch',
             f'/lobby/{lobby_id}/change-type/{lobby_type}/change-mode/{lobby_mode}/',
-            token=self.online_verified_user_2.auth.token,
+            token=self.user_2.auth.token,
         )
 
         self.assertEqual(response.status_code, 400)
@@ -320,9 +270,9 @@ class LobbyAPITestCase(mixins.SomePlayersMixin, TestCase):
         )
 
     def test_lobby_enter(self):
-        lobby_1 = Lobby.create(self.online_verified_user_1.id)
+        lobby_1 = Lobby.create(self.user_1.id)
         lobby_1.set_public()
-        lobby_2 = Lobby.create(self.online_verified_user_2.id)
+        lobby_2 = Lobby.create(self.user_2.id)
 
         self.assertEqual(lobby_1.players_count, 1)
         self.assertEqual(lobby_2.players_count, 1)
@@ -330,7 +280,7 @@ class LobbyAPITestCase(mixins.SomePlayersMixin, TestCase):
         response = self.api.call(
             'patch',
             f'/lobby/{lobby_1.id}/enter/',
-            token=self.online_verified_user_2.auth.token,
+            token=self.user_2.auth.token,
         )
 
         self.assertEqual(response.status_code, 200)
@@ -338,8 +288,8 @@ class LobbyAPITestCase(mixins.SomePlayersMixin, TestCase):
         self.assertEqual(lobby_2.players_count, 0)
 
     def test_lobby_enter_isnt_public(self):
-        lobby_1 = Lobby.create(self.online_verified_user_1.id)
-        lobby_2 = Lobby.create(self.online_verified_user_2.id)
+        lobby_1 = Lobby.create(self.user_1.id)
+        lobby_2 = Lobby.create(self.user_2.id)
 
         self.assertEqual(lobby_1.players_count, 1)
         self.assertEqual(lobby_2.players_count, 1)
@@ -347,7 +297,7 @@ class LobbyAPITestCase(mixins.SomePlayersMixin, TestCase):
         response = self.api.call(
             'patch',
             f'/lobby/{lobby_1.id}/enter/',
-            token=self.online_verified_user_2.auth.token,
+            token=self.user_2.auth.token,
         )
 
         self.assertEqual(response.status_code, 400)
