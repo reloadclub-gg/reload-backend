9797de92f42baf2b8a67e5910ae0bb79f364b8df
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-01-13 21:06:19 -0300

    update websocket tests by fixing some old tests

--- websocket/tests/test_utils.py
+++ websocket/tests/test_utils.py
@@ -1,3 +1,6 @@
+import asyncio
+from threading import Thread
+
 from channels.layers import get_channel_layer
 from django.conf import settings
 
@@ -5,39 +8,41 @@ from accounts.tests.mixins import AccountOneMixin
 from core.tests import TestCase
 from websocket import utils
 
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+
 
 class WSUtilsTestCase(AccountOneMixin, TestCase):
+    async def listen1(self):
+        channel_layer = get_channel_layer()
+        message = await channel_layer.receive(f'{settings.GROUP_NAME_PREFIX}.group1')
+        assert message['type'] == 'send_payload'
+        assert message['payload'] == {'content': 'test'}
+        assert message['meta']['action'] == 'ws_Test'
+        assert 'timestamp' in message['meta']
+
+    async def listen2(self):
+        channel_layer = get_channel_layer()
+        message = await channel_layer.receive(f'{settings.GROUP_NAME_PREFIX}.group2')
+        assert message['type'] == 'send_payload'
+        assert message['payload'] == {'content': 'test'}
+        assert message['meta']['action'] == 'ws_Test'
+        assert 'timestamp' in message['meta']
+
+    def between_callback(self):
+        loop.run_until_complete(self.listen1())
+        loop.run_until_complete(self.listen2())
+
     async def test_ws_send(self):
         channel_layer = get_channel_layer()
-        channel_name_1 = await channel_layer.new_channel()
-        channel_name_2 = await channel_layer.new_channel()
+
+        thread1 = Thread(target=self.between_callback)
+        thread1.start()
 
         await utils.ws_send(
             'ws_Test',
             {'content': 'test'},
-            [channel_name_1, channel_name_2],
+            ['group1', 'group2'],
         )
 
-        async def listen1():
-            message = await channel_layer.receive(
-                f'{settings.GROUP_NAME_PREFIX}.{channel_name_1}'
-            )
-            assert message['type'] == 'send_payload'
-            assert message['payload'] == {'content': 'test'}
-            assert message['meta']['action'] == 'ws_Test'
-            assert 'timestamp' in message['meta']
-            await channel_layer.close_pools()
-
-        async def listen2():
-            message = await channel_layer.receive(
-                f'{settings.GROUP_NAME_PREFIX}.{channel_name_2}'
-            )
-            assert message['type'] == 'send_payload'
-            assert message['payload'] == {'content': 'test'}
-            assert message['meta']['action'] == 'ws_Test'
-            assert 'timestamp' in message['meta']
-            await channel_layer.close_pools()
-
-        await listen2()
-        await listen1()
         await channel_layer.flush()
