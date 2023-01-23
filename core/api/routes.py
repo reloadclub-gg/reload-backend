75d7d217e005da2ffdc513b21439ca3a269f1f17
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-01-23 14:51:05 -0300

    add translation to django and django-ninja

--- core/api/routes.py
+++ core/api/routes.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.utils.translation import gettext as _
 from ninja import NinjaAPI
+from ninja.errors import AuthenticationError, ValidationError
 
 from accounts.api.routes import router as accounts_router
 from matchmaking.api.routes import router as mm_router
@@ -9,6 +10,21 @@ local_env = settings.ENVIRONMENT == settings.LOCAL
 api = NinjaAPI(openapi_url=local_env and '/openapi.json' or '')
 api.add_router("/accounts/", accounts_router)
 api.add_router("/mm/", mm_router)
+
+
+@api.exception_handler(ValidationError)
+def validation_errors(request, exc):
+    errors = [
+        {'loc': error['loc'], 'msg': _(error['msg']), 'type': error['type']}
+        for error in exc.errors
+    ]
+
+    return api.create_response(request, {'detail': errors}, status=422)
+
+
+@api.exception_handler(AuthenticationError)
+def authentication_errors(request, exc):
+    return api.create_response(request, {'detail': _('Unauthorized')}, status=401)
 
 
 @api.get('')
--- core/tests/test_core.py
+++ core/tests/test_core.py
@@ -1,3 +1,8 @@
+from django.conf import settings
+from django.utils import translation
+
+from accounts.tests.mixins import UserOneMixin
+
 from . import APIClient, TestCase, cache
 
 
@@ -46,12 +51,42 @@ class CoreApiClientTestCase(TestCase):
         )
         self.assertEqual(call.request.get('token'), 'token')
 
+
+class CoreApiI18nTestCase(UserOneMixin, TestCase):
     def test_i18n(self):
         client = APIClient('/api')
+        r = client.generic('get', '')
+        i18n_response = r.json().get('i18n_check')
+
         r = client.generic(
             'get', '', HTTP_ACCEPT_LANGUAGE='pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7'
         )
+        cur_language = translation.get_language()
+        self.assertEqual(cur_language, r.json().get('language'))
         self.assertEqual(r.json().get('language'), 'pt-br')
+        self.assertEqual(r.json().get('i18n_check'), translation.gettext(i18n_response))
         self.assertEqual(
             r.json().get('i18n_check'), 'A internacionalização está funcionando.'
+        )
+
+    def test_i18n_errors_authentication(self):
+        client = APIClient('/api/accounts')
+        r = client.generic(
+            'post',
+            '/',
+            HTTP_ACCEPT_LANGUAGE='pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+        )
+        self.assertEqual(r.json().get('detail'), 'Não autorizado.')
+
+    def test_i18n_errors_validation(self):
+        self.user.auth.create_token()
+        client = APIClient('/api/accounts')
+        r = client.generic(
+            'post',
+            '/',
+            token=self.user.auth.token,
+            HTTP_ACCEPT_LANGUAGE='pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+        )
+        self.assertEqual(
+            r.json().get('detail')[0].get('msg'), 'Esse campo é obrigatório.'
         )
