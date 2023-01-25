848951f59b974ecdb353ec5a31951eab783a1137
Adriano Margarin <adriano.margarin@gmail.com>
2023-01-24 21:30:04 -0300

    Add fields in model AppSettings

--- appsettings/models.py
+++ appsettings/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 
 class AppSettings(models.Model):
+    KIND_CHOICES = (
+        ('text', _('Text')),
+        ('integer', _('Integer')),
+        ('boolean', _('Boolean')),
+    )
+
     name = models.CharField(max_length=100)
     value = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    kind = models.CharField(choices=KIND_CHOICES, default='text')
     description = models.TextField(null=True, blank=True)
 
     def __str__(self):
