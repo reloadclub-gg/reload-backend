50c33245a77ef93f5206f6eaba6d7116f441f55f
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-01-25 18:55:41 -0300

    add some props to lobby schema

--- matchmaking/api/schemas.py
+++ matchmaking/api/schemas.py
@@ -1,8 +1,45 @@
-from typing import Optional
+from typing import List, Optional
 
+from django.contrib.auth import get_user_model
 from ninja import Schema
 
+from steam import Steam
+
 from ..models import Lobby, LobbyInvite
+
+User = get_user_model()
+
+
+class LobbyPlayerSchema(Schema):
+    id: Optional[int]
+    steamid: Optional[str]
+    username: Optional[str]
+    avatar: Optional[dict]
+    is_online: Optional[bool]
+    level: Optional[str]
+
+    class Config:
+        model = User
+
+    @staticmethod
+    def resolve_steamid(obj):
+        return obj.steam_user.steamid
+
+    @staticmethod
+    def resolve_username(obj):
+        return obj.steam_user.username
+
+    @staticmethod
+    def resolve_avatar(obj):
+        return {
+            'small': Steam.build_avatar_url(obj.steam_user.avatarhash),
+            'medium': Steam.build_avatar_url(obj.steam_user.avatarhash, 'medium'),
+            'large': Steam.build_avatar_url(obj.steam_user.avatarhash, 'full'),
+        }
+
+    @staticmethod
+    def resolve_level(obj):
+        return obj.account.level
 
 
 class LobbySchema(Schema):
@@ -12,10 +49,14 @@ class LobbySchema(Schema):
     mode: int
     max_players: int
     players_ids: list
+    players: List[LobbyPlayerSchema]
+    players_count: int
+    non_owners_ids: list
     is_public: bool
     invites: list
     invited_players_ids: list
     overall: int
+    seats: int
     queue: Optional[str]
     queue_time: Optional[int]
 
@@ -23,52 +64,12 @@ class LobbySchema(Schema):
         model = Lobby
 
     @staticmethod
-    def resolve_id(obj):
-        return obj.id
-
-    @staticmethod
-    def resolve_owner_id(obj):
-        return obj.owner_id
-
-    @staticmethod
-    def resolve_lobby_type(obj):
-        return obj.lobby_type
-
-    @staticmethod
-    def resolve_mode(obj):
-        return obj.mode
-
-    @staticmethod
-    def resolve_max_players(obj):
-        return obj.max_players
-
-    @staticmethod
-    def resolve_players_ids(obj):
-        return sorted(obj.players_ids)
-
-    @staticmethod
-    def resolve_is_public(obj):
-        return obj.is_public
-
-    @staticmethod
-    def resolve_invites(obj):
-        return obj.invites
-
-    @staticmethod
-    def resolve_invited_players_ids(obj):
-        return obj.invited_players_ids
-
-    @staticmethod
-    def resolve_overall(obj):
-        return obj.overall
-
-    @staticmethod
     def resolve_queue(obj):
         return obj.queue.strftime('%Y-%m-%d %H:%M:%S') if obj.queue else None
 
     @staticmethod
-    def resolve_queue_time(obj):
-        return obj.queue_time
+    def resolve_players(obj):
+        return [User.objects.get(pk=user_id) for user_id in obj.players_ids]
 
 
 class LobbyInviteSchema(Schema):
