fceb8d6f95f5665b2a2892fcc37ac80509a06a61
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-01-29 19:55:03 -0300

    move static files to core

--- core/settings.py
+++ core/settings.py
@@ -178,6 +178,7 @@ if ENVIRONMENT != LOCAL:
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core', 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
