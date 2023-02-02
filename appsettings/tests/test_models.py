a3249ed50983b16c37e286db083a5275f242b4f7
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-02-02 11:56:04 -0300

    squash migrations and add improvements to appsettings

--- appsettings/tests/test_models.py
+++ appsettings/tests/test_models.py
@@ -1,22 +1,23 @@
 from model_bakery import baker
 
+from appsettings.models import AppSettings
 from core.tests import TestCase
 
 
 class AppSettingsTestCase(TestCase):
     def test_str(self):
-        obj = baker.make('appsettings.AppSettings', name='name')
+        obj = baker.make(AppSettings, name='name')
 
         self.assertEqual(str(obj), 'name')
 
     def test_description_isnone(self):
-        obj = baker.make('appsettings.AppSettings')
+        obj = baker.make(AppSettings)
 
         self.assertIsNone(obj.description)
 
     def test_obj(self):
         obj = baker.make(
-            'appsettings.AppSettings',
+            AppSettings,
             name='name',
             value='value',
             description='description',
@@ -25,3 +26,47 @@ class AppSettingsTestCase(TestCase):
         self.assertEqual(obj.name, 'name')
         self.assertEqual(obj.value, 'value')
         self.assertEqual(obj.description, 'description')
+
+    def test_get(self):
+        AppSettings.objects.create(kind=AppSettings.TEXT, name='name', value='test')
+        config = AppSettings.get('name')
+        self.assertEqual(config, 'test')
+
+    def test_set_text(self):
+        AppSettings.set_text(name='name', value='test')
+        config = AppSettings.get('name')
+        self.assertEqual(config, 'test')
+
+    def test_set_boolean(self):
+        AppSettings.set_bool(name='name', value=True)
+        config = AppSettings.get('name')
+        self.assertEqual(config, True)
+
+        AppSettings.set_bool(name='name2', value=False)
+        config = AppSettings.get('name2')
+        self.assertEqual(config, False)
+
+    def test_set_int(self):
+        AppSettings.set_int(name='name', value=1)
+        config = AppSettings.get('name')
+        self.assertEqual(config, 1)
+
+        AppSettings.set_int(name='name2', value=3)
+        config = AppSettings.get('name2')
+        self.assertEqual(config, 3)
+
+    def test_save(self):
+        config = AppSettings(name='name', kind=AppSettings.TEXT, value='test')
+        config.save()
+        value = AppSettings.get('name')
+        self.assertEqual(value, 'test')
+
+        config = AppSettings(name='name2', kind=AppSettings.BOOLEAN, value=False)
+        config.save()
+        value = AppSettings.get('name2')
+        self.assertEqual(value, False)
+
+        config = AppSettings(name='name3', kind=AppSettings.INTEGER, value=5)
+        config.save()
+        value = AppSettings.get('name3')
+        self.assertEqual(value, 5)
--- appsettings/tests/test_services.py
+++ appsettings/tests/test_services.py
@@ -6,9 +6,6 @@ from ..services import check_invite_required
 
 class CheckInviteRequiredTestCase(TestCase):
     def test_check_invite_required_has_true(self):
-        self.assertTrue(check_invite_required())
-
-    def test_check_invite_required_has_false(self):
-        AppSettings.objects.filter(name='Invite Required').update(value='0')
-
         self.assertFalse(check_invite_required())
+        AppSettings.set_bool('Invite Required', True)
+        self.assertTrue(check_invite_required())
