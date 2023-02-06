a1675aa510f69f9eec7a43035d604491336be211
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-02-06 16:27:15 -0300

    add ability to bypass inactive users on social_core login

---
+++ accounts/social_urls.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.urls import path
+from social_core.utils import setting_name
+from social_django import views
+
+from accounts.views import complete
+
+extra = getattr(settings, setting_name("TRAILING_SLASH"), True) and "/" or ""
+
+app_name = "social"
+
+urlpatterns = [
+    # authentication / association
+    path(f"login/<str:backend>{extra}", views.auth, name="begin"),
+    path(f"complete/<str:backend>{extra}", complete, name="complete"),
+    # disconnection
+    path(f"disconnect/<str:backend>{extra}", views.disconnect, name="disconnect"),
+    path(
+        f"disconnect/<str:backend>/<int:association_id>{extra}",
+        views.disconnect,
+        name="disconnect_individual",
+    ),
+]
