51b262a601177eae71715637395d8b58da0ed7d3
Adriano Margarin <adriano.margarin@gmail.com>
2023-02-07 22:37:45 -0300

    Add view, template and URL admin to rendered emails

---
+++ appsettings/views.py
@@ -0,0 +1,23 @@
+import os
+
+from django.conf import settings
+from django.http.response import JsonResponse
+from django.shortcuts import render
+from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
+
+
+def emails_view(request, get_context):
+    context = {
+        'title': 'E-mails',
+        'emails': os.listdir(
+            f'{settings.BASE_DIR}/accounts/templates/accounts/emails/'
+        ),
+        **get_context(request),
+    }
+
+    return render(request, 'emails.html', context)
+
+
+def email_rendered_by_name_view(request, name):
+    return JsonResponse({'email_rendered': render_to_string(f'accounts/emails/{name}')})
