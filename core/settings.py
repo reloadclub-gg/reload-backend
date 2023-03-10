ae8ba0e0139e69fd024112e13f14970dff9bb2b4
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-03-10 18:51:15 -0300

    add new settings for team players min ready

--- core/settings.py
+++ core/settings.py
@@ -327,7 +327,10 @@ JAZZMIN_SETTINGS = {
     },
 }
 
-# Match Settings
+# Team & Match Settings
+TEAM_READY_PLAYERS_MIN = (
+    5 if TEST_MODE else config('TEAM_READY_PLAYERS_MIN', default=5, cast=int)
+)
 MATCH_READY_PLAYERS_MIN = (
     10 if TEST_MODE else config('MATCH_READY_PLAYERS_MIN', default=10, cast=int)
 )
