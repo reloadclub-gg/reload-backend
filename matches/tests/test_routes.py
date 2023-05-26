0a05de6ec2d5d5a1b8f08830f487a03c41cafe4f
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-05-26 15:20:53 -0300

    add user_id param to matches list

--- matches/tests/test_routes.py
+++ matches/tests/test_routes.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from model_bakery import baker
 
 from core.tests import APIClient, TestCase
@@ -20,3 +21,43 @@ class MatchesRoutesTestCase(mixins.TeamsMixin, TestCase):
         r = self.api.call('get', f'/{match.id}', token=self.user_1.auth.token)
         self.assertEqual(r.status_code, 200)
         self.assertEqual(r.json(), schemas.MatchSchema.from_orm(match).dict())
+
+    def test_match_list(self):
+        server = baker.make(models.Server)
+        match1 = baker.make(
+            models.Match,
+            server=server,
+            status=models.Match.Status.FINISHED,
+            end_date=timezone.now(),
+        )
+        team1 = match1.matchteam_set.create(name=self.team1.name, score=10)
+        match1.matchteam_set.create(name=self.team2.name, score=6)
+        baker.make(models.MatchPlayer, team=team1, user=self.user_1)
+
+        match2 = baker.make(
+            models.Match,
+            server=server,
+            status=models.Match.Status.FINISHED,
+            end_date=timezone.now(),
+        )
+        team1 = match2.matchteam_set.create(name=self.team1.name, score=10)
+        match2.matchteam_set.create(name=self.team2.name, score=6)
+        baker.make(models.MatchPlayer, team=team1, user=self.user_1)
+        r = self.api.call('get', '/', token=self.user_1.auth.token)
+        self.assertEqual(r.json().get('count'), 2)
+
+        match = baker.make(
+            models.Match,
+            server=server,
+            status=models.Match.Status.FINISHED,
+            end_date=timezone.now(),
+        )
+        team1 = match.matchteam_set.create(name=self.team1.name, score=10)
+        match.matchteam_set.create(name=self.team2.name, score=6)
+        baker.make(models.MatchPlayer, team=team1, user=self.user_2)
+        r = self.api.call(
+            'get',
+            f'/?user_id={self.user_2.id}',
+            token=self.user_1.auth.token,
+        )
+        self.assertEqual(r.json().get('count'), 1)
