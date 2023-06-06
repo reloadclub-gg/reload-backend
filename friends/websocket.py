cdbfa0151dd7ed13f6e7ff538dba8d073951765a
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-06-06 19:04:43 -0300

    add websocket files specifics to their packages

---
+++ friends/websocket.py
@@ -0,0 +1,18 @@
+from asgiref.sync import async_to_sync
+from celery import shared_task
+from django.contrib.auth import get_user_model
+
+from websocket.utils import ws_send
+
+from .api import schemas
+
+User = get_user_model()
+
+
+def ws_status_update(user_id: int):
+    user = User.objects.get(pk=user_id)
+    online_friends_ids = [account.user.id for account in user.account.online_friends]
+    payload = schemas.FriendSchema.from_orm(user.account).dict()
+    return async_to_sync(ws_send)(
+        'friends/status_update', payload, groups=online_friends_ids
+    )
---
+++ notifications/websocket.py
@@ -0,0 +1,21 @@
+from asgiref.sync import async_to_sync
+from celery import shared_task
+from django.contrib.auth import get_user_model
+
+from websocket.utils import ws_send
+
+from .api.schemas import NotificationSchema
+from .models import Notification
+
+User = get_user_model()
+
+
+def ws_new_notification(notification_id: int):
+    notification = Notification.get_by_id(notification_id)
+    payload = NotificationSchema.from_orm(notification).dict()
+
+    return async_to_sync(ws_send)(
+        'notifications/add',
+        payload,
+        groups=[notification.to_user_id],
+    )
