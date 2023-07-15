587c6c930e637023db392abc595539b62eb68997
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-07-15 19:03:00 -0300

    add new task to run when a player restriction ends

--- lobbies/tasks.py
+++ lobbies/tasks.py
@@ -2,10 +2,15 @@ import time
 
 from celery import shared_task
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.utils import timezone
 
+from accounts.websocket import ws_update_user
+
 from . import models
-from .websocket import ws_queue_tick
+from .websocket import ws_queue_tick, ws_update_lobby
+
+User = get_user_model()
 
 
 @shared_task
@@ -25,3 +30,11 @@ def queue_tick(lobby_id: int):
         if not settings.TEST_MODE:
             time.sleep(1)
             queue_tick(lobby_id)
+
+
+@shared_task
+def end_player_restriction(user_id: int):
+    user = User.objects.get(pk=user_id)
+    lobby = user.account.lobby
+    ws_update_lobby(lobby)
+    ws_update_user(user)
