2c548207d9a6e4475c388b8fcad723bcb3fafe84
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-07-19 00:20:09 -0300

    update get_all method from pre_match model

--- pre_matches/models/pre_match.py
+++ pre_matches/models/pre_match.py
@@ -200,16 +200,17 @@ class PreMatch(BaseModel):
                     return PreMatch(id=match_id)
 
     @staticmethod
-    def get_all():
-        pre_matches_keys = cache.keys(f'{PreMatch.Config.CACHE_PREFIX}?')
+    def get_all() -> list[Team]:
+        """
+        Fetch and return all PreMatches on Redis db.
+        """
+        all_keys = cache.keys(f'{PreMatch.Config.CACHE_PREFIX}*')
         result = []
-        for pre_match_key in pre_matches_keys:
-            if len(pre_match_key.split(':')) == 3:
-                pre_match_id = pre_match_key.split(':')[2]
-                pre_match = PreMatch.get_by_id(pre_match_id)
-                result.append(pre_match)
+        for key in all_keys:
+            if len(key.split(':')) == 3:
+                result.append(key)
 
-        return result
+        return [PreMatch.get_by_id(key.split(':')[2]) for key in result]
 
     @staticmethod
     def get_by_player_id(player_id: int):
