15eae5768334da13ee0d29f321298115c3c7dcf2
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-07-22 02:32:53 -0300

    polish locust file

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,20 +1,18 @@
 import itertools
+import random
 
 from locust import FastHttpUser, between, task
+
+user_ids = []
 
 
 class AppUser(FastHttpUser):
     wait_time = between(1, 2.5)
-    id_auto_number = itertools.count(start=1000)
-    queued = False
-    queue_available = True
+    id_auto_number = itertools.count(start=1)
+    user = None
+    token = None
 
-    def on_start(self):
-        self.token = self.fake_signup()
-        self.verify(self.token)
-        self.user = self.auth(self.token)
-
-    def fake_signup(self):
+    def _fake_signup(self):
         email = f'email{next(self.id_auto_number)}@locust.com'
 
         with self.client.post(
@@ -22,88 +20,61 @@ class AppUser(FastHttpUser):
             json={
                 'email': email,
             },
+            name="accounts/signup/",
         ) as response:
             return response.json().get('token')
 
-    def verify(self, token: str):
+    def _verify(self, token: str):
         with self.client.post(
             '/api/accounts/verify/',
             json={
                 'verification_token': 'debug0',
             },
             headers={'Authorization': f'Bearer {token}'},
+            name="accounts/verify/",
         ) as response:
             return response.json()
 
-    def auth(self, token: str):
+    def _auth(self, token: str):
         with self.client.get(
             '/api/accounts/auth/',
             headers={'Authorization': f'Bearer {token}'},
+            name="accounts/detail/",
         ) as response:
             return response.json()
 
+    def _prepare(self):
+        if not self.token:
+            self.token = self._fake_signup()
+            self.user = self._auth(self.token)
+            self._verify(self.token)
+        else:
+            self.user = self._auth(self.token)
+            if not self.user.get('account').get('is_verified'):
+                self._verify(self.token)
+
+        if not self.user.get('id') in user_ids:
+            user_ids.append(self.user.get('id'))
+
     @task
-    def queue(self):
-        if not self.queue_available:
-            return
-
-        updated_user = self.auth(self.token)
-
-        if updated_user.get('match_id'):
-            return
-
-        lobby_id = updated_user.get('lobby_id')
-
+    def profile(self):
+        self._prepare()
+        user_id = random.choice(user_ids)
         with self.client.get(
-            f'/api/lobbies/{lobby_id}/',
+            f'/api/profiles/?user_id={user_id}',
             headers={'Authorization': f'Bearer {self.token}'},
-            name="lobbies/detail/",
-        ) as response:
-            lobby = response.json()
-
-        restricted = lobby.get('restriction_countdown')
-
-        if not self.queued and not restricted:
-            with self.client.patch(
-                f'/api/lobbies/{lobby_id}/',
-                headers={'Authorization': f'Bearer {self.token}'},
-                json={'start_queue': True},
-                name="lobbies/start_queue/",
-            ) as response:
-                self.queued = True
-                return response.json()
-        else:
-            with self.client.patch(
-                f'/api/lobbies/{lobby_id}/',
-                headers={'Authorization': f'Bearer {self.token}'},
-                json={'cancel_queue': True},
-                name="lobbies/cancel_queue/",
-            ) as response:
-                self.queued = False
-                return response.json()
+            name="profiles/detail/",
+        ):
+            pass
 
     @task
-    def lock_in_or_ready(self):
-        updated_user = self.auth(self.token)
-
-        if updated_user.get('match_id'):
-            return
-
-        if updated_user.get('pre_match_id'):
-            self.queue_available = False
+    def lobby(self):
+        self._prepare()
+        lobby_id = self.user.get('lobby_id')
+        if lobby_id:
             with self.client.get(
-                '/api/pre-matches/',
+                f'/api/lobbies/{lobby_id}/',
                 headers={'Authorization': f'Bearer {self.token}'},
-                name="pre_matches/detail/",
-            ) as response:
-                pre_match = response.json()
-
-            if pre_match and pre_match.get('state') == 'pre_start':
-                with self.client.post(
-                    '/api/pre-matches/lock-in/',
-                    headers={'Authorization': f'Bearer {self.token}'},
-                    name="pre_matches/lock-in/",
-                ) as response:
-                    return response.json()
-        else:
-            self.queue_available = True
+                name="lobbies/detail/",
+            ):
+                pass
