3a726326a9fed24f13dcae96e6e0c20e51a50a6e
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-10-27 17:00:56 -0300

    fix watch_user_status_change task

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -1,17 +1,46 @@
+import logging
+
 from celery import shared_task
 from django.contrib.auth import get_user_model
 from django.utils import timezone
+from django.utils.translation import gettext as _
 
+from accounts.websocket import ws_update_user
 from core.redis import redis_client_instance as cache
+from core.websocket import ws_create_toast
 from friends.websocket import ws_friend_update_or_create
 from lobbies.api.controller import handle_player_move
-from lobbies.models import LobbyException
+from lobbies.models import Lobby, LobbyException
 from lobbies.websocket import ws_expire_player_invites
+from pre_matches.models import PreMatch, Team
+from pre_matches.websocket import ws_pre_match_delete
 
 from . import utils, websocket
 from .models import UserLogin
 
 User = get_user_model()
+
+
+def cancel_pre_match(pre_match: PreMatch):
+    for player in pre_match.players:
+        ws_create_toast(
+            _('Match cancelled due to user not locked in.'),
+            'warning',
+            user_id=player.id,
+        )
+
+    # send ws call to lobbies to cancel that match
+    ws_pre_match_delete(pre_match)
+    for player in pre_match.players:
+        ws_update_user(player)
+        ws_friend_update_or_create(player)
+
+    # delete the pre_match and teams from Redis
+    team1 = pre_match.teams[0]
+    team2 = pre_match.teams[1]
+    PreMatch.delete(pre_match.id)
+    team1.delete()
+    team2.delete()
 
 
 @shared_task
@@ -22,21 +51,38 @@ def watch_user_status_change(user_id: int):
     """
     user = User.objects.get(pk=user_id)
     if not user.has_sessions:
+        logging.info('watch_user_status_change')
+        logging.info(user.email)
         # Expiring player invites
         ws_expire_player_invites(user)
 
         # If user has an account
         if hasattr(user, 'account'):
+            if user.account.lobby:
+                team = Team.get_by_lobby_id(user.account.lobby.id, fail_silently=True)
+                if team:
+                    team.remove_lobby(user.account.lobby.id)
+
+            pre_match = PreMatch.get_by_player_id(user.id)
+            if pre_match:
+                logging.info('cancel_pre_match')
+                cancel_pre_match(pre_match)
+
             try:
                 handle_player_move(user, user.id, delete_lobby=True)
             except LobbyException as e:
-                raise e
-
-            # Update or create friend
-            ws_friend_update_or_create(user)
+                logging.info('handle_player_move error - deleting lobby mannualy')
+                logging.info(e)
+                if user.account.lobby:
+                    Lobby.delete(user.account.lobby.id)
 
         # Expiring user session
         user.logout()
+        logging.info('user status')
+        logging.info(user.status)
+
+        # Update or create friend
+        ws_friend_update_or_create(user)
 
         # Send websocket logout message
         websocket.ws_user_logout(user.id)
