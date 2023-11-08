060ecdbee8c0f878b3d40ac9a40ca2cdf88d2100
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-11-08 13:42:34 -0300

    enable add or edit items/boxes on user admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -162,12 +162,6 @@ class UserItemAdminInline(admin.TabularInline):
     def item_is_available(self, obj):
         return obj.item.is_available
 
-    def has_change_permission(self, request, obj=None) -> bool:
-        return False
-
-    def has_add_permission(self, request, obj=None) -> bool:
-        return False
-
 
 class UserBoxAdminInline(admin.TabularInline):
     model = UserBox
@@ -185,12 +179,6 @@ class UserBoxAdminInline(admin.TabularInline):
 
     def box_is_available(self, obj):
         return obj.box.is_available
-
-    def has_change_permission(self, request, obj=None) -> bool:
-        return False
-
-    def has_add_permission(self, request, obj=None) -> bool:
-        return False
 
 
 class CustomUserStatusFilter(admin.SimpleListFilter):
