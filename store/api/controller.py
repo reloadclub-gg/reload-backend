4f72403de4685b461ffa7e2caddbca7d3f9486c2
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-11-09 12:44:51 -0300

    WIP RC endpoints

--- store/api/controller.py
+++ store/api/controller.py
@@ -1,10 +1,14 @@
+import stripe
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
+from ninja.errors import Http404, HttpError
 
 from .. import models
 from . import schemas
 
 User = get_user_model()
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 def item_update(user: User, item_id: int, payload: schemas.UserItemUpdateSchema):
@@ -22,3 +26,26 @@ def item_update(user: User, item_id: int, payload: schemas.UserItemUpdateSchema)
         item.save()
 
     return user
+
+
+def fetch_products():
+    products = stripe.Product.list().get('data')
+    return [schemas.ProductSchema.from_orm(product) for product in products]
+
+
+def start_purchase(request, payload: schemas.PurchaseSchema):
+    product = stripe.Product.retrieve(payload.product_id)
+    if not product:
+        raise Http404()
+
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            payment_method_types=['card', 'boleto'],
+            line_items=[{'price': product.get('default_price'), 'quantity': 1}],
+            mode='payment',
+            success_url=request.build_absolute_uri('/success/'),
+            cancel_url=request.build_absolute_uri('/cancel/'),
+        )
+        return {'client_secret': checkout_session.client_secret}
+    except Exception as e:
+        raise HttpError(400, str(e))
---
+++ store/tests/test_controller.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from model_bakery import baker
+
+from accounts.tests.mixins import VerifiedAccountsMixin
+from core.tests import TestCase
+
+from ..api import controller
+
+
+class StoreControllerTestCase(VerifiedAccountsMixin, TestCase):
+    def test_fetch_products(self):
+        products = controller.fetch_products()
+        self.assertTrue(len(products) > 0)
+
+    def test_start_purchase(self):
+        products = controller.fetch_products()
+
+        class Request:
+            def build_absolute_uri(self, endpoint: str):
+                return settings.FRONT_END_URL + endpoint
+
+        request = Request()
+        session_id = controller.start_purchase(request, products[0].id)
+        self.assertIsNotNone(session_id)
