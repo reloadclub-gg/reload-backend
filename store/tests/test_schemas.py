013ad761361f0f7895712882115f8e796ae7284e
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-11-13 13:57:55 -0300

    add next_rotation, object and media fields on store and items schemas

--- store/tests/test_schemas.py
+++ store/tests/test_schemas.py
@@ -1,13 +1,15 @@
 import tempfile
-from decimal import Decimal
+from datetime import timedelta
 
+from django.conf import settings
 from django.test import override_settings
+from django.utils import timezone
 from model_bakery import baker
 
 from accounts.tests.mixins import AccountOneMixin
 from core.redis import redis_client_instance as cache
 from core.tests import TestCase
-from core.utils import get_full_file_path
+from core.utils import get_full_file_path, str_to_timezone
 
 from .. import models
 from ..api import schemas
@@ -72,7 +74,7 @@ class StoreSchemaTestCase(AccountOneMixin, TestCase):
             'item_type': self.item.item_type,
             'subtype': self.item.subtype,
             'handle': self.item.handle,
-            'price': Decimal(str(self.item.price)),
+            'price': self.item.price,
             'release_date': self.item.release_date,
             'description': self.item.description,
             'discount': self.item.discount,
@@ -86,6 +88,39 @@ class StoreSchemaTestCase(AccountOneMixin, TestCase):
             'featured': self.item.featured,
             'in_use': None,
             'can_use': None,
+            'object': 'item',
+            'media': [],
+        }
+        self.assertEqual(payload, expected_payload)
+
+    def test_item_media_schema(self):
+        baker.make(models.ItemMedia, item=self.item, file=self.tmp_image, _quantity=4)
+        payload = schemas.ItemSchema.from_orm(self.item).dict()
+        expected_payload = {
+            'id': self.item.id,
+            'name': self.item.name,
+            'item_type': self.item.item_type,
+            'subtype': self.item.subtype,
+            'handle': self.item.handle,
+            'price': self.item.price,
+            'release_date': self.item.release_date,
+            'description': self.item.description,
+            'discount': self.item.discount,
+            'background_image': get_full_file_path(self.item.background_image),
+            'foreground_image': get_full_file_path(self.item.foreground_image),
+            'box': schemas.BoxSchema.from_orm(self.item.box) if self.item.box else None,
+            'box_draw_chance': self.item.box_draw_chance,
+            'collection': schemas.CollectionSchema.from_orm(self.collection)
+            if self.item.collection
+            else None,
+            'featured': self.item.featured,
+            'in_use': None,
+            'can_use': None,
+            'object': 'item',
+            'media': [
+                schemas.ItemMediaSchema.from_orm(media)
+                for media in self.item.itemmedia_set.all()
+            ],
         }
         self.assertEqual(payload, expected_payload)
 
@@ -97,7 +132,7 @@ class StoreSchemaTestCase(AccountOneMixin, TestCase):
             'item_type': self.box_item.item_type,
             'subtype': self.box_item.subtype,
             'handle': self.box_item.handle,
-            'price': Decimal(str(self.box_item.price)),
+            'price': self.box_item.price,
             'release_date': self.box_item.release_date,
             'description': self.box_item.description,
             'discount': self.box_item.discount,
@@ -111,6 +146,8 @@ class StoreSchemaTestCase(AccountOneMixin, TestCase):
             'featured': self.box_item.featured,
             'in_use': None,
             'can_use': None,
+            'object': 'item',
+            'media': [],
         }
         self.assertEqual(payload, expected_payload)
 
@@ -122,7 +159,7 @@ class StoreSchemaTestCase(AccountOneMixin, TestCase):
             'item_type': self.collection_item.item_type,
             'subtype': self.collection_item.subtype,
             'handle': self.collection_item.handle,
-            'price': Decimal(str(self.collection_item.price)),
+            'price': self.collection_item.price,
             'release_date': self.collection_item.release_date,
             'description': self.collection_item.description,
             'discount': self.collection_item.discount,
@@ -140,6 +177,8 @@ class StoreSchemaTestCase(AccountOneMixin, TestCase):
             'featured': self.collection_item.featured,
             'in_use': None,
             'can_use': None,
+            'object': 'item',
+            'media': [],
         }
         self.assertEqual(payload, expected_payload)
 
@@ -149,7 +188,7 @@ class StoreSchemaTestCase(AccountOneMixin, TestCase):
             'id': self.box.id,
             'name': self.box.name,
             'handle': self.box.handle,
-            'price': Decimal(str(self.box.price)),
+            'price': self.box.price,
             'release_date': self.box.release_date,
             'description': self.box.description,
             'discount': self.box.discount,
@@ -157,6 +196,7 @@ class StoreSchemaTestCase(AccountOneMixin, TestCase):
             'foreground_image': get_full_file_path(self.box.foreground_image),
             'featured': self.box.featured,
             'can_open': None,
+            'object': 'box',
         }
         self.assertEqual(payload, expected_payload)
 
@@ -166,13 +206,14 @@ class StoreSchemaTestCase(AccountOneMixin, TestCase):
             'id': self.collection.id,
             'name': self.collection.name,
             'handle': self.collection.handle,
-            'price': Decimal(str(self.collection.price)),
+            'price': self.collection.price,
             'release_date': self.collection.release_date,
             'description': self.collection.description,
             'discount': self.collection.discount,
             'background_image': get_full_file_path(self.collection.background_image),
             'foreground_image': get_full_file_path(self.collection.foreground_image),
             'featured': self.collection.featured,
+            'object': 'collection',
         }
         self.assertEqual(payload, expected_payload)
 
@@ -286,3 +327,9 @@ class StoreSchemaTestCase(AccountOneMixin, TestCase):
 
         payload = schemas.UserStoreSchema.from_orm(self.user).dict()
         self.assertEqual(len(payload.get('products')), 1)
+        next_rotation_date = str_to_timezone(payload.get('next_rotation'))
+        expected_rotation_date = timezone.now() + timedelta(
+            days=settings.STORE_ROTATION_DAYS
+        )
+        self.assertEqual(next_rotation_date.day, expected_rotation_date.day)
+        self.assertEqual(next_rotation_date.hour, expected_rotation_date.hour)
