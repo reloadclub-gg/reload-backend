4a414aca975c510b89757e87dd01caf1402b425d
Gabriel Gularte <gabrielfgularte@gmail.com>
2024-01-09 17:54:38 -0300

    fix error when starting mock fivem match

--- matches/tasks.py
+++ matches/tasks.py
@@ -13,24 +13,35 @@ from . import models
 
 @shared_task
 def mock_fivem_match_start(match_id: int):
+    if settings.ENVIRONMENT != settings.LOCAL and not settings.FIVEM_MATCH_MOCKS_ON:
+        return
+
     if settings.ENVIRONMENT == settings.LOCAL:
-        url = f'http://api:8000/api/matches/{match_id}/'
-        r = requests.patch(
-            url,
-            json={'status': 'running'},
-        )
-        return r
+        url = f'{settings.DOCKER_SITE_URL}/api/matches/{match_id}/'
+    else:
+        url = f'{settings.SITE_URL}/api/matches/{match_id}/'
+
+    r = requests.patch(
+        url,
+        json={'status': 'running'},
+    )
+    return r
 
 
 @shared_task
 def mock_fivem_match_cancel(match_id: int):
+    if settings.ENVIRONMENT != settings.LOCAL and not settings.FIVEM_MATCH_MOCKS_ON:
+        return
+
     if settings.ENVIRONMENT == settings.LOCAL:
-        url = f'http://api:8000/api/matches/{match_id}/'
-        r = requests.delete(
-            url,
-            json={'status': 'cancelled'},
-        )
-        return r
+        url = f'{settings.DOCKER_SITE_URL}/api/matches/{match_id}/'
+    else:
+        url = f'{settings.SITE_URL}/api/matches/{match_id}/'
+    r = requests.delete(
+        url,
+        json={'status': 'cancelled'},
+    )
+    return r
 
 
 @shared_task
