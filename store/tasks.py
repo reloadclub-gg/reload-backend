c35f49219133767dbbf9ecb687c7cd9b65d2acb4
Gabriel Gularte <gabrielfgularte@gmail.com>
2024-01-28 02:20:11 -0300

    add store task to repopulate users stores

--- store/tasks.py
+++ store/tasks.py
@@ -1,8 +1,12 @@
 from datetime import datetime
 
 from celery import shared_task
+from django.contrib.auth import get_user_model
 
+from . import models
 from .utils import send_purchase_mail
+
+User = get_user_model()
 
 
 @shared_task
@@ -12,3 +16,14 @@ def send_purchase_mail_task(
     transaction_date: datetime,
 ):
     send_purchase_mail(mail_to, transaction_id, transaction_date)
+
+
+@shared_task
+def repopulate_user_store(user_id: int):
+    try:
+        user = User.objects.get(id=user_id)
+    except User.DoesNotExsist:
+        user = None
+
+    if user:
+        models.UserStore.populate(user)
